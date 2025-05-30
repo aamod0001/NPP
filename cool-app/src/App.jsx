fa2313c9041105eac7e878aa7937cb5d0d0a5cc7
Aamod0001 <uma0tamu@gmail.com>
2025-05-30 20:05:43 +0545

    finally learned how react works

--- cool-app/src/App.jsx
+++ cool-app/src/App.jsx
@@ -3,21 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function hello_world() {
+  return ( <h1>Hello world </h1> )
+}
+
+
+
 function App() {
   const [count, setCount] = useState(0)
-
+  
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+        <h1> hello world </h1>
+        <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
